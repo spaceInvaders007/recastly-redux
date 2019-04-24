c50f17e934e636c792866568fdc54206ac32a700
shawn-chambers <chambers.shawn.89@gmail.com>
2019-04-24 16:11:37 -0700

    Made actions and API call

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,33 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  return function(dispatch) {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q}, (videos) => {
+      changeVideoList(videos);
+      changeVideo(videos[0]);
+    })
+  }
+    // return function(dispatch) {
+    //   dispatch({
+    //     type: 'CHANGE_VIDEO_LIST',
+    //     videos: data
+    //   });
+
+    // fetch(YOUTUBE_API_KEY)
+    //   // .then(response => response.json())
+    //   .then(data => dispatch({
+    //       type: 'VIDEO_RECEIVED',
+    //       payload: data
+    //     }))
+    //   .catch(error => dispatch({
+    //       type: 'VIDEO_FAILED',
+    //       payload: error
+    //     })
+    //   );
+    // }
 };
+
+
 
 export default handleVideoSearch;
